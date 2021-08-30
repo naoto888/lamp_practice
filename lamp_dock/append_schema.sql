5c7edd5ed9ba3b41bad651aa02182f1232571f22
sudo naoto <black820gd@gmail.com>
2021-08-30 22:21:38 +0900

    購入履歴、明細テーブルの作成

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -6,12 +6,16 @@ CREATE TABLE `history` (
   `user_id` int(11) NOT NULL,
   `total_price` int(11) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  primary key(order_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `details` (
+  `detail_id` int(11) AUTO_INCREMENT NOT NULL,
   `order_id` int(11) NOT NULL,
   `amount` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  primary key(detail_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
